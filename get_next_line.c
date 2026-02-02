be709a3f720300e8c9c48acc9f3424ed81f208cd
Victor Abad Romero <vabad-ro@c2r11s1.42madrid.com>
2026-02-02 19:50:14 +0100

    empezando proyecto

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: vabad-ro <vabad-ro@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 19:44:11 by vabad-ro          #+#    #+#             */
-/*   Updated: 2026/02/02 19:47:29 by vabad-ro         ###   ########.fr       */
+/*   Updated: 2026/02/02 19:49:42 by vabad-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main(void)
 {
-	return (0)
+	printf("hola");
+	return (0);
 }
